83ac4e9ef713065b7a3c1412c4530004b62acab2
Kelly Castelo <kawokkun@gmail.com>
2023-02-09 18:47:25 -0300

    corrigindo bug

--- javamos-decolar/src/Main.java
+++ javamos-decolar/src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+package javamos_decolar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello... world?");
+    }
 }
