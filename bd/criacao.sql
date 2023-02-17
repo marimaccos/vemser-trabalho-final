52be5d77b92cbe737449cf29528d33a9e0fc4e74
Kelly Castelo <kawokkun@gmail.com>
2023-02-17 18:31:42 -0300

    adiciona constraints faltantes

--- bd/criacao.sql
+++ bd/criacao.sql
@@ -1,16 +1,16 @@
 CREATE TABLE "Usuario" (
   "id_usuario" NUMBER,
-  "login" VARCHAR(20),
-  "senha" VARCHAR(20),
-  "nome" VARCHAR(100),
-  "tipo" CHAR(1),
+  "login" VARCHAR(20) UNIQUE NOT NULL,
+  "senha" VARCHAR(20) NOT NULL,
+  "nome" VARCHAR(100) NOT NULL,
+  "tipo" CHAR(1) NOT NULL,
   PRIMARY KEY ("id_usuario")
 );
 
 CREATE TABLE "Comprador" (
   "id_comprador" NUMBER,
-  "cpf" CHAR(11),
-  "id_usuario" NUMBER,
+  "cpf" CHAR(11) UNIQUE NOT NULL,
+  "id_usuario" NUMBER NOT NULL,
   PRIMARY KEY ("id_comprador"),
   CONSTRAINT "FK_Comprador.id_usuario"
     FOREIGN KEY ("id_usuario")
@@ -19,9 +19,9 @@ CREATE TABLE "Comprador" (
 
 CREATE TABLE "Companhia" (
   "id_companhia" NUMBER,
-  "cnpj" CHAR(14),
-  "nome_fantasia" VARCHAR(100),
-  "id_usuario" NUMBER,
+  "cnpj" CHAR(14) UNIQUE NOT NULL,
+  "nome_fantasia" VARCHAR(100) NOT NULL,
+  "id_usuario" NUMBER NOT NULL,
   PRIMARY KEY ("id_companhia"),
   CONSTRAINT "FK_Companhia.id_usuario"
     FOREIGN KEY ("id_usuario")
@@ -30,10 +30,10 @@ CREATE TABLE "Companhia" (
 
 CREATE TABLE "Venda" (
   "id_venda" NUMBER,
-  "status" VARCHAR2(20),
-  "data" DATE,
-  "id_companhia" NUMBER,
-  "id_comprador" NUMBER,
+  "status" VARCHAR2(20) NOT NULL,
+  "data" DATE NOT NULL,
+  "id_companhia" NUMBER NOT NULL,
+  "id_comprador" NUMBER NOT NULL,
   PRIMARY KEY ("id_venda"),
   CONSTRAINT "FK_Venda.id_comprador"
     FOREIGN KEY ("id_comprador")
@@ -45,9 +45,9 @@ CREATE TABLE "Venda" (
 
 CREATE TABLE "Trecho" (
   "id_trecho" NUMBER,
-  "origem" CHAR(3),
-  "destino" CHAR(3),
-  "id_companhia" NUMBER,
+  "origem" CHAR(3) NOT NULL,
+  "destino" CHAR(3) NOT NULL,
+  "id_companhia" NUMBER NOT NULL,
   PRIMARY KEY ("id_trecho"),
   CONSTRAINT "FK_Trecho.id_companhia"
     FOREIGN KEY ("id_companhia")
@@ -56,13 +56,13 @@ CREATE TABLE "Trecho" (
 
 CREATE TABLE "Passagem" (
   "id_passagem" NUMBER,
-  "codigo" NUMBER,
-  "data_partida" DATE,
-  "data_chegada" DATE,
-  "disponivel" CHAR(1),
-  "valor" NUMBER,
-  "id_trecho" NUMBER,
-  "id_venda" NUMBER,
+  "codigo" NUMBER UNIQUE NOT NULL,
+  "data_partida" DATE NOT NULL,
+  "data_chegada" DATE NOT NULL,
+  "disponivel" CHAR(1) NOT NULL,
+  "valor" NUMBER NOT NULL,
+  "id_trecho" NUMBER NOT NULL,
+  "id_venda" NUMBER NOT NULL,
   PRIMARY KEY ("id_passagem"),
   CONSTRAINT "FK_Passagem.id_venda"
     FOREIGN KEY ("id_venda")
