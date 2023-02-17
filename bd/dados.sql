eaa8277e61546b2a75cd2044c67a4580dff5883e
Kelly Castelo <kawokkun@gmail.com>
2023-02-17 19:00:13 -0300

    edita ordem de dados e adicona valor a passagem

--- bd/dados.sql
+++ bd/dados.sql
@@ -24,16 +24,16 @@ VALUES (VEM_SER.seq_comprador.nextval, '11958516007', 2);
 INSERT INTO trecho (id_trecho, origem, destino, id_companhia) 
 VALUES (VEM_SER.seq_comprador.nextval, 'BEL', 'CWB', 1);
 
--- passagens --
--- [0] - FALSE 
--- [1] - TRUE
-
-INSERT INTO passagem (id_passagem, codigo, data_partida, data_chegada, disponivel, valor, id_trecho, id_venda) 
-VALUES (VEM_SER.seq_passagem.nextval, '123456', 
-TO_DATE('17-02-2023', 'dd-mm-yyyy'), 
-TO_DATE('18-02-2023', 'dd-mm-yyyy'), 0, 13, 1, 1);
-
 -- vendas --
 
 INSERT INTO venda (id_venda, status, data_venda, id_companhia, id_comprador) 
 VALUES (VEM_SER.seq_venda.nextval, 'CONCLUÍDO', TO_DATE(CURRENT_DATE, 'dd-mm-yyyy'), 1, 1);
+
+-- passagens --
+-- [0] - false
+-- [1] - true
+
+INSERT INTO passagem (id_passagem, codigo, data_partida, data_chegada, disponivel, valor, id_trecho, id_venda) 
+VALUES (VEM_SER.seq_passagem.nextval, '123456', 
+TO_DATE('17-02-2023', 'dd-mm-yyyy'), 
+TO_DATE('18-02-2023', 'dd-mm-yyyy'), 0, 200.9, 1, 1);
