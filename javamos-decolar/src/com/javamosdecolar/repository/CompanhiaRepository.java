cfe6557d2209e175b55cf18b681c683f3565f213
Kelly Castelo <kawokkun@gmail.com>
2023-02-24 16:29:35 -0300

    refatora repository companhia

--- javamos-decolar/src/com/javamosdecolar/repository/CompanhiaRepository.java
+++ javamos-decolar/src/com/javamosdecolar/repository/CompanhiaRepository.java
@@ -27,7 +27,7 @@ public class CompanhiaRepository {
         }
     }
 
-    public Companhia adicionar(Companhia companhia, Integer idUsuario) throws DatabaseException {
+    public Companhia adicionar(Companhia companhia) throws DatabaseException {
         Connection conexao = null;
 
         try {
@@ -37,17 +37,15 @@ public class CompanhiaRepository {
             companhia.setIdCompanhia(proximoId);
 
             String sql = "INSERT INTO COMPANHIA \n" +
-                    "(ID_COMPRADOR, LOGIN, SENHA, NOME, TIPO, CPF, NOME_FANTASIA)\n" +
-                    "VALUES(?, ?, ?, ?, 1, ?, ?)";
+                    "(ID_COMPRADOR, CNPJ, NOME_FANTASIA, ID_USUARIO)\n" +
+                    "VALUES(?, ?, ?, ?)";
 
             PreparedStatement preparedStatement = conexao.prepareStatement(sql);
 
             preparedStatement.setInt(1, companhia.getIdCompanhia());
-            preparedStatement.setString(2, companhia.getLogin());
-            preparedStatement.setString(3, companhia.getSenha());
-            preparedStatement.setString(4, companhia.getNome());
-            preparedStatement.setString(5, companhia.getCnpj());
-            preparedStatement.setString(6, companhia.getNomeFantasia());
+            preparedStatement.setString(2, companhia.getCnpj());
+            preparedStatement.setString(3, companhia.getNomeFantasia());
+            preparedStatement.setInt(4, companhia.getIdUsuario());
 
             int result = preparedStatement.executeUpdate();
             System.out.println("adicionarCompanhia.res=" + result);
@@ -65,111 +63,6 @@ public class CompanhiaRepository {
             }
         }
     }
-    public List<Companhia> listar() throws DatabaseException {
-        List<Companhia> companhias = new ArrayList<>();
-        Connection conexao = null;
-
-        try{
-            conexao = ConexaoBancoDeDados.getConnection();
-            Statement statement = conexao.createStatement();
-
-            String sql = "SELECT * FROM COMPANHIA";
-
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while(resultSet.next()){
-                Companhia companhia = new Companhia();
-                companhia.setIdCompanhia(resultSet.getInt("id_companhia"));
-                companhia.setLogin(resultSet.getString("login"));
-                companhia.setSenha(resultSet.getString("senha"));
-                companhia.setNome(resultSet.getString("nome"));
-                companhia.setTipoUsuario(TipoUsuario.ofTipo(resultSet.getInt("tipo")));
-                companhia.setCnpj(resultSet.getString("cnpj"));
-                companhia.setNomeFantasia(resultSet.getString("nome_fantasia"));
-                companhias.add(companhia);
-            }
-        } catch (SQLException e) {
-            throw new DatabaseException(e.getCause());
-        } finally {
-            try {
-                if (conexao != null) {
-                    conexao.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return companhias;
-    }
-
-    @Override
-    public boolean editar(Integer id, Companhia companhia) throws DatabaseException {
-        Connection conexao = null;
-        try {
-            conexao = ConexaoBancoDeDados.getConnection();
-
-            StringBuilder sql = new StringBuilder();
-            sql.append("UPDATE COMPANHIA SET ");
-            sql.append("login = ?,");
-            sql.append("senha = ?,");
-            sql.append("nome = ?, ");
-            sql.append("cnpj = ?");
-            sql.append("nome_fantasia = ?");
-            sql.append("WHERE id_companhia = ?");
-
-            PreparedStatement statement = conexao.prepareStatement(sql.toString());
-
-            statement.setString(1, companhia.getLogin());
-            statement.setString(2, companhia.getSenha());
-            statement.setString(3, companhia.getNome());
-            statement.setString(4, companhia.getCnpj());
-            statement.setString(5, companhia.getNomeFantasia());
-            statement.setInt(6, id);
-
-            int result = statement.executeUpdate();
-            System.out.println("editarCompanhia.res=" + result);
-
-            return result > 0;
-        } catch (SQLException e) {
-            throw new DatabaseException(e.getCause());
-        } finally {
-            try {
-                if (conexao != null) {
-                    conexao.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public boolean remover(Integer id) throws DatabaseException {
-        Connection conexao = null;
-        try{
-            conexao = ConexaoBancoDeDados.getConnection();
-
-            String sql = "DELETE FROM COMPANHIA WHERE id_companhia = ?";
-
-            PreparedStatement statement = conexao.prepareStatement(sql);
-
-            statement.setInt(1, id);
-
-            int result = statement.executeUpdate();
-            System.out.println("removerCompanhiaPorId.res=" + result);
-
-            return result > 0;
-        } catch (SQLException e) {
-            throw new DatabaseException(e.getCause());
-        } finally {
-            try {
-                if (conexao != null) {
-                    conexao.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     public Optional<Companhia> buscaCompanhiaPorNome (String nome) throws DatabaseException {
         Companhia companhiaPesquisa = new Companhia();
@@ -177,7 +70,10 @@ public class CompanhiaRepository {
         try{
             conexao = ConexaoBancoDeDados.getConnection();
 
-            String sql = "SELECT * FROM COMPANHIA WHERE nome = ?";
+            String sql = "SELECT ID_COMPANHIA, u.ID_USUARIO, LOGIN, NOME, CNPJ, NOME_FANTASIA FROM COMPANHIA c \n" +
+                    "INNER JOIN\n" +
+                    "USUARIO u ON c.ID_USUARIO = u.ID_USUARIO AND u.NOME = ?";
+
             PreparedStatement statement = conexao.prepareStatement(sql);
 
             statement.setString(1, nome);
@@ -186,7 +82,6 @@ public class CompanhiaRepository {
 
             companhiaPesquisa.setIdCompanhia(resultSet.getInt("id_comprador"));
             companhiaPesquisa.setLogin(resultSet.getString("login"));
-            companhiaPesquisa.setSenha(resultSet.getString("senha"));
             companhiaPesquisa.setNome(resultSet.getString("nome"));
             companhiaPesquisa.setTipoUsuario(TipoUsuario.ofTipo(resultSet.getInt("tipo")));
             companhiaPesquisa.setCnpj(resultSet.getString("cpf"));
@@ -217,7 +112,9 @@ public class CompanhiaRepository {
         try{
             conexao = ConexaoBancoDeDados.getConnection();
 
-            String sql = "SELECT * FROM COMPANHIA WHERE id_usuario = ?";
+            String sql = "SELECT ID_COMPANHIA, u.ID_USUARIO, LOGIN, SENHA, NOME, CNPJ, NOME_FANTASIA FROM COMPANHIA c \n" +
+                    "INNER JOIN\n" +
+                    "USUARIO u ON c.ID_USUARIO = u.ID_USUARIO WHERE c.ID_USUARIO = ?";
             PreparedStatement statement = conexao.prepareStatement(sql);
 
             statement.setInt(1, idUsuario);
