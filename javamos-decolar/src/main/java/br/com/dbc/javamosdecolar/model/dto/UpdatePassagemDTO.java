4f62cb6ee3f0583b1a1c779afef26612780c45db
Kelly Castelo <kawokkun@gmail.com>
2023-03-01 21:51:02 -0300

    adiciona primeira implementacao de passagem

---
+++ javamos-decolar/src/main/java/br/com/dbc/javamosdecolar/model/dto/UpdatePassagemDTO.java
@@ -0,0 +1,43 @@
+package br.com.dbc.javamosdecolar.model.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class UpdatePassagemDTO {
+    private LocalDateTime dataPartida;
+    private LocalDateTime dataChegada;
+    private BigDecimal valor;
+    private Integer trechoId;
+
+    public LocalDateTime getDataPartida() {
+        return dataPartida;
+    }
+
+    public void setDataPartida(LocalDateTime dataPartida) {
+        this.dataPartida = dataPartida;
+    }
+
+    public LocalDateTime getDataChegada() {
+        return dataChegada;
+    }
+
+    public void setDataChegada(LocalDateTime dataChegada) {
+        this.dataChegada = dataChegada;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Integer getTrechoId() {
+        return trechoId;
+    }
+
+    public void setTrechoId(Integer trechoId) {
+        this.trechoId = trechoId;
+    }
+}
