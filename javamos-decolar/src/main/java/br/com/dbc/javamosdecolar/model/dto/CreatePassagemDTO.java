0696133e1a260fd378b3bb1b2636b84355f7b3bb
Kelly Castelo <kawokkun@gmail.com>
2023-03-02 14:15:37 -0300

    adiciona validacao para datas nos dtos de passagem

--- javamos-decolar/src/main/java/br/com/dbc/javamosdecolar/model/dto/CreatePassagemDTO.java
+++ javamos-decolar/src/main/java/br/com/dbc/javamosdecolar/model/dto/CreatePassagemDTO.java
@@ -3,6 +3,7 @@ package br.com.dbc.javamosdecolar.model.dto;
 import br.com.dbc.javamosdecolar.utils.CustomRegex;
 import lombok.Data;
 
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
@@ -12,11 +13,13 @@ public class CreatePassagemDTO {
     @NotNull(message = "O campo dataPartida não pode estar nulo!")
     @Pattern(message = "O campo dataPartida deve ser no formato dd/MM/yyyy hh:mm!",
             regexp = CustomRegex.DATE_REGEX_PATTERN)
+    @FutureOrPresent(message = "O campo dataPartida deve ser atual ou futuro!")
     private String dataPartida;
 
     @NotNull(message = "O campo dataChegada não pode estar nulo!")
     @Pattern(message = "O campo dataChegada deve ser no formato dd/MM/yyyy hh:mm!",
             regexp = CustomRegex.DATE_REGEX_PATTERN)
+    @FutureOrPresent(message = "O campo dataChegada deve ser atual ou futuro!")
     private String dataChegada;
 
     @NotNull(message = "O campo valor não pode estar nulo!")
--- javamos-decolar/src/main/java/br/com/dbc/javamosdecolar/model/dto/UpdatePassagemDTO.java
+++ javamos-decolar/src/main/java/br/com/dbc/javamosdecolar/model/dto/UpdatePassagemDTO.java
@@ -3,6 +3,7 @@ package br.com.dbc.javamosdecolar.model.dto;
 import br.com.dbc.javamosdecolar.utils.CustomRegex;
 import lombok.Data;
 
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
@@ -10,9 +11,11 @@ import java.math.BigDecimal;
 public class UpdatePassagemDTO {
     @Pattern(message = "O campo dataChegada deve ser no formato dd/MM/yyyy hh:mm!",
             regexp = CustomRegex.DATE_REGEX_PATTERN)
+    @FutureOrPresent(message = "O campo dataPartida deve ser atual ou futuro!")
     private String dataPartida;
     @Pattern(message = "O campo dataChegada deve ser no formato dd/MM/yyyy hh:mm!",
             regexp = CustomRegex.DATE_REGEX_PATTERN)
+    @FutureOrPresent(message = "O campo dataChegada deve ser atual ou futuro!")
     private String dataChegada;
     private BigDecimal valor;
     private Integer idTrecho;
